03d8e91ba6f45d6dc097fc2a7f316a9461729885
ohad1881 <tbhani123@gmail.com>
2024-10-15 16:27:01 +0300

    Resolved merge conflictss

--- controller.js
+++ controller.js
@@ -1468,7 +1468,7 @@ searchBar.addEventListener("keyup", async function (event) {
   }
 });
 const newFeature = function () {
-  console.log("welcome,this is a test,2222");
+  console.log("welcome,this is a test,22224");
 };
 newFeature();
 
